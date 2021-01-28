8dfbd4fd32df6c95d40aea5bc4cdec438bd9674d
Sophia Hadash <sophiahadash@gmail.com>
2021-01-28 22:19:25 +0100

    qrfeed bin

--- setup-host.sh
+++ setup-host.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt-get update -qqy
-sudo apt-get install v4l2loopback-dkms python python-pip -y
+sudo apt-get install v4l2loopback-dkms python python-pip imagemagick -y
 sudo modprobe v4l2loopback
 pip install scipy pillow v4l2 numpy
